9388d9d8f6573ab0270ef45c4f697422706f1f16
Eduardo Hernández Soto <hseduardo@gmail.com>
2023-01-21 17:20:30 -0600

    page course

---
+++ components/courses/Course.jsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+export default function Course({ img, alt, title, p1, p2, time, subjects }) {
+  return (
+    <div className="mx-16 tablet:mx-8 phone:mx-4 flex justify-center items-center flex-wrap gap-16">
+      <div className="max-w-[600px]">
+        <Image
+          src={img}
+          alt={alt}
+          className="img_full"
+          width={714}
+          height={540}
+          priority
+        />
+      </div>
+      <div className="flex flex-col items-stretch gap-6 max-w-[480px]">
+        <h3 className="mb-0 uppercase text-color_primary_2">{title}</h3>
+        <p className="text-color_primary_2 text-lg font-normal leading-[34px]">
+          {p1}
+        </p>
+        <p className="text-color_primary_2 text-lg font-normal leading-[34px]">
+          {p2}
+        </p>
+        <p className="text-color_primary_2 text-lg font-normal leading-[34px]">
+          {time} <br /> {subjects}
+        </p>
+      </div>
+    </div>
+  );
+}
